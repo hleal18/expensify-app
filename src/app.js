41a7c7b663beedd179c914124f85abaaaefd11b5
Humberto Leal <humberto.adolfo@hotmail.com>
2017-12-07 01:00:09 -0500

    Clase 80: se crean componentes para cada página y se hacen los respectivos import para la que reune todas las páginas AppRouter.

--- src/app.js
+++ src/app.js
@@ -1,72 +1,7 @@
 import React, { Children } from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import AppRouter from "./routers/AppRouter";
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-
-//Cada Route significa un página.
-//Si se quieren tres páginas, hay que hacer tres Routes.
-const ExpenseDashboardPage = () => (
-    <div>
-        This is from my dashboard component
-    </div>
-);
-
-const AddExpensePage = () => (
-    <div>
-        This is from my add expense component
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        This is from my edit expense page component.
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        This is from my help page component.
-    </div>
-);
-
-const NotFoundPage = () => (
-    <div>
-        404! - <Link to="/">Go home</Link>
-    </div>
-);
-
-//Ahora se usa NavLink.
-//Ofrece:
-//la identificación mediante una clase. ideal para aplicar estilos css.
-//así como el link que coincide con la URL actual.
-//hay que aplicar el mismo condicionamiento de Route, debido a que luego
-//pone en negrita link que no está del todo activo.
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    </header>
-);
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Header />
-            <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true} />
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage} />
-                <Route component={NotFoundPage} />
-            </Switch>
-        </div>
-
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter/>, document.getElementById('app'));
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+    <div>
+        This is from my edit expense page component.
+    </div>
+);
+
+export default EditExpensePage;
