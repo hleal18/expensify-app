511d7321e5d9e20d2cd729d34870d08657a939a6
Humberto Leal <humberto.adolfo@hotmail.com>
2017-12-08 16:09:50 -0500

    Clase 92: se hace una demostración con un ejemplo de la utilidad de combinereducers al gestionar el estado de dos propiedades principales, expenses y filters, de forma que cada una tenga su propio reducer y administrar estados complejos se simplifique asignando la responsabilidad a reducers más específicos, lo cual se logra en el ejemplo realizado.

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,53 @@
 import { createStore, combineReducers } from 'redux';
 
+//Se van a crear reducers que gestionen el estado.
+
+//ADD_EXPENSE
+//REMOVE_EXPENSE
+//EDIT_EXPENSE
+//SET_TEXT_FILTER
+//SORT_BY_DATE
+//SORT_BY_AMOUNT
+//SET_START_DATE
+//SET_END_DATE
+
+//EXPENSES REDUCER
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+
+};
+
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+}
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+
+console.log(store.getState());
+
+//Dos reducers para cada estado importante, los objetos expenses y filters.
+//Luego se combinan para hacer el principal.
 const demoState = {
     expenses: [{
         id: 'pijasdfhwer',
@@ -11,7 +59,7 @@ const demoState = {
     filters: {
         text: 'rent',
         sortBy: 'amount', //date or amount
-        sstartDate: undefined,
+        startDate: undefined,
         endDate: undefined
     }
 }
