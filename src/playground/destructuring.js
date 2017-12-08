80856e90f55e42c76e38371dd168fba4ef9fd28d
Humberto Leal <humberto.adolfo@hotmail.com>
2017-12-08 14:57:13 -0500

    Clase 89: se observa la misma característica de destructuring ahora con arreglos.

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,4 @@
+//OBJECT DESTRUCTURING
 // console.log('destructuring');
 
 // const person = {
@@ -30,13 +31,31 @@
 //     console.log('It\'s ' + temp + ' in ' + city + '.');
 // }
 
-const book = {
-    title: 'Ego is the enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
+// const book = {
+//     title: 'Ego is the enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
 
-const {name: publisherName = 'Self-published'} = book.publisher
-console.log(publisherName);
+// const {name: publisherName = 'Self-published'} = book.publisher
+// console.log(publisherName);
+
+//ARRAY DESTRUCTURING
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+
+//Coincide con posiciones de acuerdo a como estén guardadas en el arreglo.
+//const [street, city, state, zip] = address;
+//Se pueden saltar algunos items. De forma que solo se destructuring los necesarios.
+//No hay renombración.
+//Si hay defaults.
+// const [, city, state = 'New York'] = address;
+
+// console.log('You are in ' + city + ' ' + state + '.');
+
+const item = ['Coffe (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [itemName, , mediumPrice] = item;
+
+console.log('A medium ' +  itemName + ' costs ' + mediumPrice);
