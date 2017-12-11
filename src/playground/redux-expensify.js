fcc8936bfd4e543e95fc9b0051ae3c43a16f8053
Humberto Leal <humberto.adolfo@hotmail.com>
2017-12-11 11:25:09 -0500

    Clase 94: se finaliza la generación de los accionadores de expense y se comienza con el edit filter text de los filtros. Se instala el plugin para el operador spread en objetos por medio de babel y se usa en los reducers.

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -27,8 +27,19 @@ const removeExpense = ({ id } = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
 });
+
 //EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates    
+});
+
 //SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
 //SORT_BY_DATE
 //SORT_BY_AMOUNT
 //SET_START_DATE
@@ -45,11 +56,22 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
             return [
                 ...state,
                 action.expense
-            ];
-            break;
+            ];            
         case 'REMOVE_EXPENSE':
-            return state.filter(({ id }) => id !== action.id);
-            break;
+            return state.filter(({ id }) => id !== action.id);            
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    //Se ponen los atributos que ya posee expense
+                    //Se sobreescriben los que poseen updates del Action generator.
+                    return {
+                        ...expense,
+                        ...action.updates
+                    };
+                } else {
+                    return expense;
+                }
+            });            
         default:
             return state;
     }
@@ -65,6 +87,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            };            
         default:
             return state;
     }
@@ -85,6 +112,10 @@ const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffe', amount: 300 }));
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
 
 //Dos reducers para cada estado importante, los objetos expenses y filters.
 //Luego se combinan para hacer el principal.
@@ -103,3 +134,16 @@ const demoState = {
         endDate: undefined
     }
 }
+
+const user = {
+    name: 'Juan',
+    age: 24
+};
+
+//Se agregan campos y se sobreescriben los valores de algunos, siempre y cuando se agreguen despues del
+//spread operator.
+console.log({
+    ...user,
+    location: 'Philadelphia',
+    age: 27
+});
