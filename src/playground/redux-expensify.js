ec08ac49df242621f8c48022bcab010fdf3799e5
Humberto Leal <humberto.adolfo@hotmail.com>
2017-12-11 15:07:24 -0500

    Clase 99: se concluye la administración de los cambios de estado de los expenses y los filters mediante el uso de redux.

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -140,6 +140,30 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
     }
 };
 
+//milliseconds are counting in milliseconds.
+// 0 = January 1st 1970 (unix epoch) at midnight
+
+//Get visible expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+    return expenses.filter((expense) => {
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+        const endDateMatch = typeof endDate !== 'numbert' || expense.createdAt <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+
+
+        return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if (sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1;
+        } else if (sortBy == 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
+    });
+};
+
+// Store creation
+
 const store = createStore(
     combineReducers({
         expenses: expensesReducer,
@@ -148,11 +172,13 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-    console.log(store.getState());
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
 });
 
-// const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
-// const expenseTwo = store.dispatch(addExpense({ description: 'Coffe', amount: 300 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffe', amount: 300, createdAt: 55 }));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
@@ -160,12 +186,12 @@ store.subscribe(() => {
 // store.dispatch(setTextFilter('rent'));
 // store.dispatch(setTextFilter());
 
-// store.dispatch(sortByAmount());
+store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 
-store.dispatch(setStartDate(125));
-store.dispatch(setStartDate());
-store.dispatch(setEndDate(1250));
+// store.dispatch(setStartDate(0));
+// store.dispatch(setStartDate());
+// store.dispatch(setEndDate(1250));
 
 //Dos reducers para cada estado importante, los objetos expenses y filters.
 //Luego se combinan para hacer el principal.
