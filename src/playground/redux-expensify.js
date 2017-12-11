790336c52722afd24ef756ae05d719aba702fc2d
Humberto Leal <humberto.adolfo@hotmail.com>
2017-12-11 10:54:20 -0500

    Clase 93: Se realiza el Action de Add expense y remove expense. Se explica el es6 operador de Spread en arreglos, de forma que simplifique la modificación de arreglos sin incurrir en la manipulación directa de los datos de state.

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,9 +1,32 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
 //Se van a crear reducers que gestionen el estado.
 
 //ADD_EXPENSE
+const addExpense = (
+    { 
+        description = '', 
+        note = '', 
+        amount = 0, 
+        createdAt = 0 
+    } = {}
+) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
+
 //REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
 //EDIT_EXPENSE
 //SET_TEXT_FILTER
 //SORT_BY_DATE
@@ -17,6 +40,16 @@ const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            //Se puede usar el es6 spread operator en arreglos.
+            return [
+                ...state,
+                action.expense
+            ];
+            break;
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id }) => id !== action.id);
+            break;
         default:
             return state;
     }
@@ -44,7 +77,14 @@ const store = createStore(
     })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffe', amount: 300 }));
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 //Dos reducers para cada estado importante, los objetos expenses y filters.
 //Luego se combinan para hacer el principal.
