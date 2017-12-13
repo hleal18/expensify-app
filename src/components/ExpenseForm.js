c1ddd91d93399a9e3293d46c98c55399c0f3d0f8
Humberto Leal <humberto.adolfo@hotmail.com>
2017-12-13 09:30:48 -0500

    Clase 106: se instala moment, para gestionar fechas fácilmente y react-dates de airbnb, solamente un componente que permite elegir una fecha. Se implementan en el ExpenseForm de forma que se pueda agregar una fecha para el expense creado.

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,4 +1,23 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+//El css relacionado con el singledatepicker.
+import 'react-dates/lib/css/_datepicker.css';
+
+// Se recibe una instancia de moment.
+// tiene el momento actual.
+// const now = moment();
+// imprime el objeto
+// console.log(now);
+// imprime la fecha en formato
+// console.log(now.format());
+// imprime el mes recortado.
+// console.log(now.format('MMM'));
+// imprime el mes recortado con el dia del mes.
+// console.log(now.format('MMM Do'));
+// imprime el mes recortado con el dia del mes y el año en cuatro digitos.
+// console.log(now.format('MMM Do, YYYY'));
+
 
 //Se busca aprovechar el estado interno del componente de los datos de los inputs
 //y cuando se haya finalizado, esos datos del estado, se guardarán en redux mediante
@@ -7,18 +26,20 @@ class ExpenseForm extends Component {
     state = {
         description: '',
         amount: '',
-        note: ''
+        note: '',
+        createdAt: moment(),
+        calendarFocused: false
     };
     //Se enlaza la descripción del estado con la que escribe el usuario.
     //se mantiene guardado el estado en todo momento.
     onDescriptionChange = (e) => {
         const description = e.target.value;
         this.setState(() => ({ description }));
-    }
+    };
     onNoteChange = (e) => {
         const note = e.target.value;
         this.setState(() => ({ note }));
-    }
+    };
     //Se busca que para el input de amount, se acepten numeros de hasta dos decimales
     //nada más.
     onAmountChange = (e) => {
@@ -26,10 +47,16 @@ class ExpenseForm extends Component {
         //Se usa una expresión regular. (regular expressiones)
         //es un tema grande a consultar.
         //Se busca la documentación en regex101.com
-        if (amount.match(/^\d*(\.\d{0,2})?$/))  {
+        if (amount.match(/^\d*(\.\d{0,2})?$/)) {
             this.setState(() => ({ amount }));
         }
-    }
+    };
+    onDateChange = (createdAt) => {
+        this.setState(() => ({ createdAt }));
+    };
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({ calendarFocused: focused }))
+    };
     render() {
         return (
             <div>
@@ -46,6 +73,14 @@ class ExpenseForm extends Component {
                         placeholder="Amount"
                         value={this.state.amount}
                         onChange={this.onAmountChange}
+                    />
+                    <SingleDatePicker
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={() => false}
                     />
                     <textarea
                         placeholder="Add a note for your expense (optional)"
