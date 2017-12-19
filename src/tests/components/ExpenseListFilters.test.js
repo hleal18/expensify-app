31304890c5e5238fc0b199340eb6f27d6798db41
Humberto Leal <humberto.adolfo@hotmail.com>
2017-12-18 19:12:51 -0500

    Clase 127: se finalizan los test cases con la terminación de el componente ExpenseListFilters.

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -34,3 +34,32 @@ test('should render ExpenseListFilters with alt data correctly', () => {
     });
     expect(wrapper).toMatchSnapshot();
 });
+
+//SE EVALUAN CADA UNA DE LAS FUNCIONES QUE LLEVA A CABO EL COMPONENTE.
+test('should handle text change', () => {
+    const textInput = 'cuotas';
+    wrapper.find('input').simulate('change', { target: { value: altFilters.text }});
+    expect(setTextFilter).toHaveBeenLastCalledWith(altFilters.text);
+});
+
+test('should sort by date', () => {
+    wrapper.find('select').simulate('change', { target: { value: filters.sortBy }});
+    expect(sortByDate).toHaveBeenCalled();
+})
+
+test('should sort by amount', () => {
+    wrapper.find('select').simulate('change', { target: { value: altFilters.sortBy }});
+    expect(sortByAmount).toHaveBeenCalled();
+});
+
+test('should handle date changes', () => {
+    wrapper.find('DateRangePicker').prop('onDatesChange')({ startDate: altFilters.startDate, endDate: altFilters.endDate });
+    expect(setStartDate).toHaveBeenLastCalledWith(altFilters.startDate);
+    expect(setEndDate).toHaveBeenLastCalledWith(altFilters.endDate);
+});
+
+test('should handle data changes', () => {
+    const calendarFocused = 'startDate';
+    wrapper.find('DateRangePicker').prop('onFocusChange')(calendarFocused);
+    expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
+});
