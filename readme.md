dc35978617ee9852c5ca6f8925652396d431a532
Humberto Leal <humberto.adolfo@hotmail.com>
2017-12-19 14:45:39 -0500

    Actualización de comandos, readme y app.js

---
+++ readme.md
@@ -0,0 +1,7 @@
+#Git Commands
+
+git init - Create a new git repo 
+git status - View the changes to your project code
+git add - Add files to staging area (run: git add . , if you want to add all the files to the staging area.)
+git commit - Creates a new commit with files from staging area
+git log - View recent commits
--- src/app.js
+++ src/app.js
@@ -13,18 +13,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill.', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-store.dispatch(setTextFilter('bill'));
-
-setTimeout(() => {
-    store.dispatch(setTextFilter('water'));
-}, 3000)
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
 //Provider permite proveer el Store (donde se guardan los estados de la app)
 //a todos los componentes React que conforman la app.
 const jsx = (
