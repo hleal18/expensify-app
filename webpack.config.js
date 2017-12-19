818b8a19b1bccee7eec542eec0f2650209c85d5a
Humberto Leal <humberto.adolfo@hotmail.com>
2017-12-19 15:42:39 -0500

    Clase 133: se agrega el env production flag al script de package json. También se pasa a función el código del webpack y recibe como parámetro el env, que le indica si está siendo ejecutado en modo producción o no.

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,60 @@
 const path = require('path');
-console.log(__dirname);
 
-console.log(path.join(__dirname, 'public'));
-
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        //Se usan rules para definir como usar los loader.
-        //Tomar un jsx y convertirlo ej es6.
-        rules: [{
-            loader: 'babel-loader',
-            //Se usan regular expressions. (Consultar).
-            //Se encarga de comprobar si el archivo analizado
-            //es de extensión .js
-            //solo cuando se cumple este criterio, se ejecuta babel.
-            test: /\.js$/,
-            //no se ejecuta para los archivos en node_modules.
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            //Permite especificar un arreglo de loaders.
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
-};
-
-//loader
+//Devuelve la configuración del webpack.
+//Se configura una función debido a que permite ser llamada
+//con argumentos que permiten modificar el comportamiento
+//de acuerdo a lo que se necesite.
 /**
- * permite modificar el comportamiento de webpack cada vez
- * que ve un archivo con extensión específica ej: (archivo js).
- * Cada vez que vea un archivo js, ejecute babel.
- * Despues de instalar babel-core y babel-loader. se agrega
- * la propiedad module al objeto exports.
- * Despues, hay que crear un archivo de configuración específico
- * para babel. Se llama .babelrc
+ * Se busca que haya una forma de identificar si webpack
+ * se está ejecutando en production mode o no. 
  */
+module.exports = (env) => {
+    //Al ejecuta un comando que accione el webpack, se muestra en
+    //consola los datos especificados por el comando.
+    console.log('env', env);
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            //Se usan rules para definir como usar los loader.
+            //Tomar un jsx y convertirlo ej es6.
+            rules: [{
+                loader: 'babel-loader',
+                //Se usan regular expressions. (Consultar).
+                //Se encarga de comprobar si el archivo analizado
+                //es de extensión .js
+                //solo cuando se cumple este criterio, se ejecuta babel.
+                test: /\.js$/,
+                //no se ejecuta para los archivos en node_modules.
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                //Permite especificar un arreglo de loaders.
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        devtool: 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
+    
+    //loader
+    /**
+     * permite modificar el comportamiento de webpack cada vez
+     * que ve un archivo con extensión específica ej: (archivo js).
+     * Cada vez que vea un archivo js, ejecute babel.
+     * Despues de instalar babel-core y babel-loader. se agrega
+     * la propiedad module al objeto exports.
+     * Despues, hay que crear un archivo de configuración específico
+     * para babel. Se llama .babelrc
+     */
+};
