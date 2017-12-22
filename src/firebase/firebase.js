f57c21c507827243387c45c4c9ff00d8fbdf0fe0
Humberto Leal <humberto.adolfo@hotmail.com>
2017-12-22 16:06:39 -0500

    Clase 143: se profundiza más en las funcionalidades de la database de firebase y la naturaleza de las consultas.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -17,9 +17,46 @@ const config = {
 firebase.initializeApp(config);
 
 //Se accede a la database
-//Se obtiene la referencia de la raíz de la db.
 //Se pasa un objeto con datos que permitan testar si la conexión
 //es correcta.
-firebase.database().ref().set({
-    name: 'Humberto'
+const database = firebase.database();
+
+//Ref permite referenciar una parte de la BD.
+//Si no se especifica se manda a la root de la BD.
+//Se pueden mandar diferentes parametro al set.
+database.ref().set({
+    name: 'Humberto',
+    age: 19,
+    isSingle: true,
+    location: {
+        city: 'Cartagena',
+        country: 'Colombia'
+    }
 });
+
+//Al realizar set, se ponen los datos en la bd, sin importar los que ya estaban guardados.
+//Es decir, solo se mostrará This is my data.
+//database.ref().set('This is my data');
+
+//Cuando se quiere actualizar un dato, se pone un parámetro a ref.
+//especificando que parte de la bd se desea actualizar.
+database.ref('age').set(23);
+//Para referenciar un campo de un objeto
+database.ref('location/city').set('New York');
+
+//Se añade un nuevo campo a la raíz de la BD.
+database.ref('attributes').set({
+    height: 163,
+    weight: 43
+});
+//Se puede usar lo siguiente, o el anterior.
+// database.ref().update({
+//     attributes: {
+//         height: 162,
+//         weigth: 43
+//     }
+// });
+
+//Las consultas anteriores son asíncronas. No se ejecutan de forma secuencial debido a la tarea
+//que deben realizar para gestionar operaciones con el servidor.
+console.log('I made a request to change the data');
