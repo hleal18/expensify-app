78739b4065934b170b101e94aca2bb1bb019ddb1
Humberto Leal <humberto.adolfo@hotmail.com>
2017-12-28 17:43:02 -0500

    Clase 148: se consiguen datos de la bd mediante suscripcion y desuscripcion general (ruta).

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,6 +2,7 @@
 //desde la variable definida como 'firebase'.
 //Es recomendado usar esta sintaxis por firebase.
 import * as firebase from 'firebase';
+import { setTimeout } from 'timers';
 
 //El objeto es provisto por firebase.
 const config = {
@@ -21,34 +22,82 @@ firebase.initializeApp(config);
 //es correcta.
 const database = firebase.database();
 
+//LECTURAS A LA BASE DE DATOS
+
+//Si se quieren notificaciones de cambios.
+//se usa on, para escuchar los posibles cambios.
+//La funcion callback se ejecuta cada vez que hay cambios.
+database.ref().on('value', (snapshot) => {
+    console.log(snapshot.val());
+});
+
+//Se prueba que 3.5s el callback se ejecuta de nuevo.
+setTimeout(() => {
+    database.ref('age').set(29);
+}, 3500);
+
+//Se desuscribe de los cambios en firebase.
+setTimeout(() => {
+    database.ref().off();
+}, 7000);
+
+setTimeout(() => {
+    database.ref('age').set(30);
+}, 10500);
+
+
+//once obtiene los datos una sola vez.
+//Todos los datos de la base de datos.
+// database.ref()
+//     .once('value')
+//     .then((snapshot) => {
+//         //Snapshot contiene los datos pedidos.
+//         const val = snapshot.val();
+//         console.log(val);
+//     })
+//     .catch((e) => {
+//         console.log('Error fetching the data', e);
+//     });
+// //Especificando una ruta
+// database.ref('location/city')
+//     .once('value')
+//     .then((snapshot) => {
+//         //Snapshot contiene los datos pedidos.
+//         const val = snapshot.val();
+//         console.log(val);
+//     })
+//     .catch((e) => {
+//         console.log('Error fetching the data', e);
+//     });
+
 //Ref permite referenciar una parte de la BD.
 //Si no se especifica se manda a la root de la BD.
 //Se pueden mandar diferentes parametro al set.
-database.ref().set({
-    name: 'Humberto',
-    age: 19,
-    stressLevel: 6,
-    job: {
-        title: 'Software developer',
-        company: 'Google'
-    },
-    location: {
-        city: 'Cartagena',
-        country: 'Colombia'
-    }
-}).then(() => {
-    console.log('Data is saved.');
-}).catch((e) => {
-    console.log('This failed. ', e);;
-});
+// database.ref().set({
+//     name: 'Humberto',
+//     age: 19,
+//     stressLevel: 6,
+//     job: {
+//         title: 'Software developer',
+//         company: 'Google'
+//     },
+//     location: {
+//         city: 'Cartagena',
+//         country: 'Colombia'
+//     }
+// }).then(() => {
+//     console.log('Data is saved.');
+// }).catch((e) => {
+//     console.log('This failed. ', e);;
+// });
 
 //Se usa update para actualizar muchos datos en una invocación.
 //Solo actualiza al nivel raíz. Si se hace a algo anidado, no funcionará como se espera.
-database.ref().update({
-    stressLevel: 9,
-    'job/company': 'Amazon',
-    'location/city': 'Seattle'
-});
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+// });
 
 //Se remueve el atributo isSingle
 // database.ref('/isSingle').remove().then(() => {
