d725693dacc5b9305118e46ea5303b0529a2dc56
Humberto Leal <humberto.adolfo@hotmail.com>
2017-12-29 18:12:39 -0500

    Clase 153: corrección de test case componente AddExpensePage.

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,10 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import { startAddExpense, editExpense, removeExpense } from '../../actions/expenses';
+import expenses from '../fixtures/expenses';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+//Se le pasa un array de middlewares.
+const createMockStore = configureMockStore([thunk]);
 
 //test case para remover expenses.
 test('should set-up remove expense action object', () => {
@@ -24,39 +30,45 @@ test('should set up edit expense action object', () => {
 
 //Test case para agregar un expense con datos provistos.
 test('should set up add expense action object with provided values', () => {
-    //Se crea el objeto modelo que se agregará y se comparará.
-    const expenseData = {
-        description: 'Rent',
-        amount: 109500,
-        createdAt: 1000,
-        note: 'This was last months rent'
-    };
     //se obtiene el valor de retorno de la función (esta vez contiene un id generado)
-    const action = addExpense(expenseData);
+    const action = addExpense(expenses[2]);
     expect(action).toEqual({
         type: 'ADD_EXPENSE',
-        expense: {
-            ...expenseData,
-            //debido a que es aleatorio el id, se usa una función que compara
-            //que el tipo analizado sea el que se quiere, en este caso
-            //un string.
-            id: expect.any(String)
-        }
+        expense: expenses[2]
     })
+});
+
+//Se busca usar mock store, para comprobar los datos guardados.
+//Se usa redux-mock-store para testear
+test('should add expense to database and store', () => {
+    const store = createMockStore({});
+    const expenseData = {
+        description: 'Mouse',
+        amount: 3000,
+        note: 'This one is better',
+        createdAt: 1000
+    };
+
+    //
+    store.dispatch(startAddExpense(expenseData))
+});
+
+test('should add expense with defaults to database and store', () => {
+
 });
 
 //Una prueba unitaria para los valores por defecto, cuando no se provee un objeto
 //a añadir sino uno por defecto.
-test('should set up the ad expense action object with default values', () => {
-    const action = addExpense();
-    expect(action).toEqual({
-        type: 'ADD_EXPENSE',
-        expense: {
-            description: '',
-            note: '',
-            amount: 0,
-            createdAt: 0,
-            id: expect.any(String)
-        }
-    })
-});
+// test('should set up the ad expense action object with default values', () => {
+//     const action = addExpense();
+//     expect(action).toEqual({
+//         type: 'ADD_EXPENSE',
+//         expense: {
+//             description: '',
+//             note: '',
+//             amount: 0,
+//             createdAt: 0,
+//             id: expect.any(String)
+//         }
+//     })
+// });
