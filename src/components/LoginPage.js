fe89431f22f241f86f71c4ecf30f2d91572c8d34
Humberto Leal <humberto.adolfo@hotmail.com>
2018-01-02 10:09:31 -0500

    Clase 172: se finaliza el estilizado del LoginPage y se inicia el del dashboard.

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,9 +7,9 @@ export class LoginPage extends React.Component {
         return (
             <div className="box-layout">
                 <div className="box-layout__box">
-                    <h1 className="box-layout__title">Expensify App</h1>
+                    <h1 className="box-layout__title">Expensify</h1>
                     <p>It's time to get your expenses under control.</p>
-                    <button onClick={this.props.startLogin}>Login</button>                
+                    <button className="button" onClick={this.props.startLogin}>Login with google</button>                
                 </div>                
             </div>
         );
