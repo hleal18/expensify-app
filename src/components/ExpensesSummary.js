a93af7e640a1017b1ee6ca9c92d275b555430cec
Humberto Leal <humberto.adolfo@hotmail.com>
2018-01-02 16:15:06 -0500

    Clase 173: se estiliza la seccion de la barra superior, se modifican los links para crear un expense y su estilizados en el dashboard.

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import selectExpensesTotal from '../selectors/expense-total';
 import getVisibleExpenses from '../selectors/expenses';
 import numeral from 'numeral';
@@ -8,8 +9,13 @@ export const ExpensesSummary = (props) => {
     const expenseWord = props.expenseCount === 1 ? 'expense' : 'expenses';
     const formattedExpenseTotal = numeral(props.expensesTotal/100).format('$0,0.00');
     return (
-        <div>
-            <h1>Viewing {props.expenseCount} {expenseWord} totalling {formattedExpenseTotal}</h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{props.expenseCount}</span> {expenseWord} totalling <span>{formattedExpenseTotal}</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
         </div>
     );
 };
